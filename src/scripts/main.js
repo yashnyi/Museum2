e350903424ed7521c69dd5c7752eca1ed833ff6c
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-01-26 17:24:57 +0200

    menu added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Menu toggle:
+
+document.querySelector('.sandwich').addEventListener('click', function(e) {
+  const menu = document.querySelector('.menu');
+  const sandwich = document.querySelector('.sandwich__button');
+
+  menu.classList.toggle('menu--active');
+  sandwich.classList.toggle('sandwich__button--active');
+});
