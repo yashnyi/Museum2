979a1800a89e9bd45c14333248be6a65ff5bd4fb
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-01-27 14:24:47 +0200

    language selector added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
-// Menu toggle:
+// Lang selector:
+const selectBlock = document.querySelector('.select');
+const currentLang = document.querySelector('.select__lang');
+const listLang = document.querySelector('.select__lang-list');
+const iconLang = document.querySelector('.select__icon');
 
+selectBlock.addEventListener('click', function() {
+  currentLang.classList.toggle('select__lang--active');
+  iconLang.classList.toggle('select__icon--active');
+  listLang.classList.toggle('select__lang-list--active');
+  selectBlock.classList.toggle('select--active');
+});
+
+// Change lang:
+listLang.addEventListener('click', function(e) {
+  if (e.target.classList.contains('select--ua')) {
+    currentLang.textContent = 'UA';
+  } else if (e.target.classList.contains('select--en')) {
+    currentLang.textContent = 'EN';
+  }
+});
+
+// Menu toggle:
 document.querySelector('.sandwich').addEventListener('click', function(e) {
   const menu = document.querySelector('.menu');
   const sandwich = document.querySelector('.sandwich__button');
